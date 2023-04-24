b7e3a48e141c8ead73448807cc309301c0f2014c
Luciefifi <niyomutonilucie@gmail.com>
2023-04-24 10:53:46 +0300

    add reducer

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //creating action
 // an action is an abject with a type element
 
+const { bindActionCreators } = require("redux")
+
 const CAKE_OREDERED = 'CAKE_ORDERED'
 
 //action creator : a function which returs the object / action
@@ -11,4 +13,26 @@ function orederCake () {
             quantity: 1,
         }
     
+}
+
+//reducer
+//(previous_state , action)
+
+const initialState = {
+    numOfCakes : 10,
+    anotherProperty: 0
+}
+
+const reducer = (state = initialState , action) =>{
+    switch(action.type)
+   {
+    case CAKE_OREDERED:
+        return{
+            ...state,
+            numOfCakes: state.numOfCakes -1
+
+        }
+        default:
+            return state
+   }
 }
