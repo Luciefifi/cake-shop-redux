14e7df8a5c6c9807fb09b65eeb27711903f5264c
Luciefifi <niyomutonilucie@gmail.com>
2023-04-26 10:00:43 +0300

    fetching data

--- react-rtk-demo/src/features/user/UserView.jsx
+++ react-rtk-demo/src/features/user/UserView.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useEffect}from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
 export const UserView = ()=>{
+    const user = useSelector((state)=>state.user)
+    const dispatch=useDispatch()
+  useEffect(()=>{
+    dispatch(fetchUsers())
+  }, [])
     return(
         <div>
             <h1>list of all users</h1>
-        </div>
+            {user.loading && <div>loading...</div>}
+            {!user.loading && user.error ? <div>Error: {user.error} </div> : null}
+            {!user.loading && user.users.length? (
+            <ul>
+                {user.users.map(user=>
+                    <li key={user.id} >{user.name}</li>
+                )
+                 } </ul>) :null}
+
+            </div>
     )
 }
