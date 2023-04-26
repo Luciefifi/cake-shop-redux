f566a1fd0b44c30c9704a727c1933f76b44440ef
Luciefifi <niyomutonilucie@gmail.com>
2023-04-26 07:57:58 +0300

    use selector hook

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 export const CakeView = ()=>{
+     const numOfCakes = useSelector((state)=> state.cake.numOfCake
+    )
     return(
         <div>
-            <h1>Number of Cakes</h1>
+            <h1>Number of Cakes {numOfCakes}</h1>
             <button>Order Cake</button>
             <button>Restock Cakes</button>
         </div>
--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 export const IcecreamView = ()=>{
+    const icecreams = useSelector((state)=>state.icecream.numOfIcecream)
     return(
         <div>
-            <h1>Number of Ice creams</h1>
+            <h1>Number of Ice creams {icecreams}</h1>
             <button>Order Ice cream</button>
             <button>Restock Ice Creams</button>
         </div>
